8197d022c296b5c9d7379abcace18548153958e5
sunyunbo <sunyunbo@axera-tech.com>
2025-08-03 16:34:55 +0800

    refactor

--- ax_dsl/axnn/n2n/__init__.py
+++
@@ -1,9 +0,0 @@
-from ax_dsl.axnn.language import Tensor
-
-
-def all_reduce(input: Tensor, output: Tensor, core_id: int):
-    pass
-
-
-def all_gather(input: Tensor, output: Tensor, core_id: int):
-    pass
--- qwen3/test.py
+++
@@ -1,7 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model = AutoModelForCausalLM.from_pretrained(
-    "/data/sunyunbo/workspace/data/Qwen/Qwen3-30B-A3B"
-)
-print(type(model))
